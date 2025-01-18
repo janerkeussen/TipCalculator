a48a51bf4096722afb4be692000f95e7a619683c
janerkeussen <zhanerke.usen@gmail.com>
2025-01-18 12:40:46 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Zhanerke Ussen on 18/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
