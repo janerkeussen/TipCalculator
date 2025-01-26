6da56c6bd2af4cee66ec06e0cc0752aa8aa178f5
janerkeussen <zhanerke.usen@gmail.com>
2025-01-25 16:26:58 -0800

    Add more test cases

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -45,6 +45,57 @@ final class TipCalculatorTests: XCTestCase  {
         }.store(in: &cancellables)
     }
     
+    func testResultWithoutTipFor2Person() {
+        //Given
+        let bill: Double = 100
+        let tip: Tip = .none
+        let split: Int = 2
+        
+        //When
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        
+        //Then
+        sut.transform(input: input).updatingViewPublisher.sink { result in
+            XCTAssertEqual(result.totalBill, 100.0)
+            XCTAssertTrue(result.totalTip == 0.0)
+            XCTAssertEqual(result.amountPerPerson, 50)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith10PercentTipFor2Person() {
+        //Given
+        let bill: Double = 100
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        
+        //When
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        
+        //Then
+        sut.transform(input: input).updatingViewPublisher.sink { result in
+            XCTAssertEqual(result.totalBill, 110.0)
+            XCTAssertTrue(result.totalTip == 10)
+            XCTAssertEqual(result.amountPerPerson, 55)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipFor4Person() {
+        //Given
+        let bill: Double = 200
+        let tip: Tip = .custom(value: 15)
+        let split: Int = 4
+        
+        //When
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        
+        //Then
+        sut.transform(input: input).updatingViewPublisher.sink { result in
+            XCTAssertEqual(result.totalBill, 215.0)
+            XCTAssertTrue(result.totalTip == 15)
+            XCTAssertEqual(result.amountPerPerson, (result.totalBill/Double(split)))
+        }.store(in: &cancellables)
+    }
+    
     private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorViewModel.Input {
         .init(
             billPublisher: Just(bill).eraseToAnyPublisher(),
